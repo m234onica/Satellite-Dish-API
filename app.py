37583dd8ef09fafea88f92b2370ece12aae8d5b9
joycehsu <m234onica@gmail.com>
2019-12-05 21:16:54 +0800

    修改data顯示格式與回傳指定欄位，解決只能顯示一筆資料的問題

--- app.py
+++ app.py
@@ -1,59 +1,64 @@
-from flask import Flask, request, jsonify
-from main import create_app
+from flask import Flask, request, jsonify, json
+from main import create_app, db_session
 from sqlalchemy import or_
 from models import event
 from datetime import date, timedelta, time
+import ast
 
 app = create_app()
 
-
-# fake_data = [
-#     {
-#         "img": "https://satellite-l5yx88bg3.now.sh/fakePics/1.jpeg",
-#         "title": "Urban Nomad Freakout Music Fest | 2019 遊牧怪奇音樂祭",
-#         "region": "北部",
-#         "date": {
-#             "start": "2019-11-01",
-#             "end": "2019-12-10"
-#         },
-#         "display_date": "11/20（一）、11/30（一）、12/5 （一）",
-#         "address": "新光三越 台北信義新天地(台北市台北市信義區松高路19號)",
-#         "link": "https://accupass",
-#         "desc": "dddddddd"
-#     }
-# ]
-
-
-@app.route('/events/<categories>/<years>/<months>/<days>', methods=['GET'])
+@app.route('/api/events/<categories>/<years>/<months>/<days>', methods=['GET'])
 def get_events(categories, years, months, days):
-    
+  
   urlDate = date(int(years), int(months), int(days))
   searchEndDate = urlDate - timedelta(days=-7)
   
   event_data = event.query.filter_by(category=categories).\
-      filter(~(or_(event.end_date < urlDate, event.start_date > searchEndDate))).all()
-  print(type(event_data))
+      filter(~(or_(event.end_date < urlDate, event.start_date > searchEndDate))).\
+      with_entities(
+        event.img, 
+        event.link, 
+        event.title, 
+        event.start_date, 
+        event.end_date, 
+        event.location, 
+        event.desc, 
+        event.display_date).\
+      all()
+  # print(event_data)
+  
   if not event_data:
     return 'No data'
-  for data in event_data:
-    result = [
-      {
-        "img": data.img,
-        "title": data.title,
-        "date": {
-            "start": data.start_date,
-            "end": data.end_date,
-        },
-        "display_date": data.display_date,
-        "address": data.location,
-        "link": data.link,
-        "desc": data.desc
-      }
-    ]
-    return jsonify(result)
 
+  list = [dict(zip(result.keys(), result)) for result in event_data]
+  return jsonify(list)
+
+@app.route('/api/banner', methods=['GET'])
+def get_all_banner():
+  all_banner = event.query.with_entities(
+      event.img,
+      event.id,
+      event.title,
+      event.location,
+      event.desc,
+      event.display_date).all()
+  print(all_banner)
+  list = [dict(zip(result.keys(), result)) for result in all_banner]
+  return jsonify(list)
   
-    
-    
+
+@app.route('/api/<categories>/banner', methods=['GET'])
+def get_banner(categories):
+  event_banner = event.query.filter_by(category=categories).with_entities(
+      event.img,
+      event.id,
+      event.title,
+      event.location,
+      event.desc,
+      event.display_date).all()
+  print(event_banner)
+  list = [dict(zip(result.keys(), result)) for result in event_banner]
+  return jsonify(list)
+  
 if __name__ == '__main__':
     app.run(port='5002', debug=True)
