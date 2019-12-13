4158f835bb8b2ae6c7063b4f3630fd075216cbfd
joycehsu <m234onica@gmail.com>
2019-12-13 18:55:46 +0800

    新增event_list.html，將所有event顯示在event lsit中

--- app.py
+++ app.py
@@ -9,17 +9,50 @@ from storage import decode_and_get_url
 app = create_app()
 
 
+@app.route('/')
+def index():
+    return render_template('event_list.html')
+
+@app.route('/api/<int:id>')
+def get_each_event(id):
+  event = Event.query.filter_by(id=id).first()
+
+  result = []
+  data = {
+      "index": event.id,
+      "img": event.img,
+      "title": event.title,
+      # "category": event.category,
+      "region": event.region,
+      "date": {
+          "start": event.start_date,
+          "end": event.end_date,
+      },
+      "display_date": event.display_date,
+      "note": event.note,
+      "address": event.location,
+      "link": event.link,
+      "desc": event.desc,
+      "reporter": {
+          "name": event.reporter_name,
+          "email": event.reporter_email,
+          "phone": event.reporter_phone
+      }
+  }
+  result.append(data)
+  return jsonify(result)
+
 @app.route('/api/events/<category>/<int:year>/<int:month>/<int:day>', methods=['GET'])
 def get_events(category, year, month, day):
 
   if category not in CATEGORY_DB:
-    return 'Category is not found.', 404
+    return 'Category is not found.', 400
   
   try:
     url_start_date = date(year, month, day)
   except ValueError as n:
     print(n)
-    return 'The date is not correct.', 404
+    return 'The date is not correct.', 400
 
   result = []
   url_end_date = url_start_date + timedelta(days=7)
@@ -70,7 +103,7 @@ def get_all_banner():
 def get_banner(category):
    
   if category not in CATEGORY_DB:
-    return 'Category is not found.', 404
+    return 'Category is not found.', 400
 
   result = []
   event_banners = Event.query.filter_by(category=category).all()
@@ -121,6 +154,34 @@ def create_event():
       db.session.commit()
 
     return 'Saved to the database!'
+
+  if request.method == 'GET':
+    all_events = Event.query.all()
+    result = []
+    for event in all_events:
+      data = {
+        "index": event.id,
+        "img": event.img,
+        "title": event.title,
+        # "category": event.category,
+        "region": event.region,
+        "date": {
+            "start": event.start_date,
+            "end": event.end_date,
+        },
+        "display_date": event.display_date,
+        "note": event.note,
+        "address": event.location,
+        "link": event.link,
+        "desc": event.desc,
+        "reporter": {
+          "name": event.reporter_name,
+          "email": event.reporter_email,
+          "phone": event.reporter_phone
+        }
+      }
+      result.append(data)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port='5002', debug=True)
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+BUCKET_NAME = 'satellite-l5yx88bg3'
+
+CATEGORY_DB = ['music', 'visual_art', 'market', 'theater']
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
   FLASK_APP = os.environ.get('FLASK_APP')
@@ -10,8 +15,3 @@ class Config:
   SQLALCHEMY_TRACK_MODIFICATIONS = True
   JSON_AS_ASCII = False
 
-# PROJECT_ID = 'momoka-244303'
-BUCKET_NAME = 'satellite-l5yx88bg3'
-
-CATEGORY_DB = ['music', 'visual_art', 'market', 'theater']
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
