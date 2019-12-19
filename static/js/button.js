4a4eaf63e95d64c0476b242f6b2dc1c50d5e7179
joycehsu <m234onica@gmail.com>
2019-12-19 13:38:41 +0800

    修正sidebar hightlight css無法運行的bug

--- static/js/button.js
+++ static/js/button.js
@@ -1,11 +1,3 @@
 var closeWindows = function () {
   document.getElementById('event-detail').style.display = 'none';
 }
-
-function myFunction() {
-    var x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1)
-        x.className += " w3-show";
-    else
-        x.className = x.className.replace(" w3-show", "");
-    }
