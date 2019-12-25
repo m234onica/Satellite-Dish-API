22b36c8dea14ff722121b962e4c31a0af8656f11
joycehsu <m234onica@gmail.com>
2019-12-25 14:49:36 +0800

    重構程式碼 structure.py: 資料架構 data.py: 將資料append成需要顯示的架構

--- app.py
+++ app.py
@@ -7,74 +7,39 @@ from init import create_app, db
 from config import CATEGORY_DB, ALLOWED_EXTENSIONS, PER_PAGE
 from models import Event, category_Enum
 from storage import decode_and_get_url
-
+from data import data
 import json
 
 app = create_app()
 
+#Backstage management
 @app.route('/')
 def index():
   return redirect(url_for('event'))
 
+
 @app.route('/event', methods=['GET'])
 def event():
   page = request.args.get('page', 1, type=int)
-  pagination = Event.query.order_by(Event.id).\
-                paginate(page,per_page=PER_PAGE,error_out=True,max_per_page=None)
+  pagination = Event.query.order_by(Event.start_date.asc()).\
+      paginate(page, per_page=PER_PAGE, error_out=True, max_per_page=None)
   all_events = pagination.items
-                   
-  result = []
-  for event in all_events:
-    event_category = event.category
-    data = {
-      "index": event.id,
-      "img": event.img,
-      "title": event.title,
-      "category": event_category.name,
-      "region": event.region,
-      "date": {
-        "start": event.start_date.strftime("%Y-%m-%d"),
-        "end": event.end_date.strftime("%Y-%m-%d"),
-      },
-        "display_date": event.display_date,
-      "note": event.note,
-      "address": event.location,
-      "link": event.link,
-      "desc": event.desc,
-      "reporter": {
-          "name": event.reporter_name,
-          "email": event.reporter_email,
-        "phone": event.reporter_phone
-      },
-        "status": event.status,
-        "banner":{
-          "home": event.home_banner,
-          "category": event.category_banner,
-        }
-    }
-    result.append(data)
-  return render_template("event.html", result=result, pagination=pagination)
+  return render_template("event.html", 
+                          result=data('events', all_events), 
+                          pagination=pagination)
 
 
-@app.route('/all_banner')
+@app.route('/banner')
 def get_all_banner():
   page = request.args.get('page', 1, type=int)
   pagination = Event.query.filter_by(status=1).order_by(Event.id).\
-                  paginate(page, per_page=PER_PAGE, error_out=True, max_per_page=None)
+      paginate(page, per_page=PER_PAGE, error_out=True,
+               max_per_page=None)
   all_banners = pagination.items
+  return render_template("all_banner.html", 
+                          result=data('banners', all_banners), 
+                          pagination=pagination)
 
-  result = []
-  for banner in all_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
-  return render_template("all_banner.html", result=result, pagination=pagination)
 
 @app.route('/home_banner', methods=['GET'])
 def home_banner():
@@ -82,18 +47,9 @@ def home_banner():
   pagination = Event.query.filter_by(status=1).filter_by(home_banner=1).order_by(Event.id).\
                   paginate(page, per_page=PER_PAGE, error_out=True, max_per_page=None)
   home_banners = pagination.items
-  result = []
-  for banner in home_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
-  return render_template("home.html", result=result, pagination=pagination)
+  return render_template("home.html", 
+                          result=data('banners', home_banners), 
+                          pagination=pagination)
 
 
 @app.route('/category_banner/<category>', methods=['GET'])
@@ -106,53 +62,25 @@ def category_banner(category):
                   filter_by(category_banner=1). filter_by(category=category). order_by(Event.id).\
                   paginate(page, per_page=PER_PAGE, error_out=True, max_per_page=None)
   category_banners = pagination.items
-  result = []
-  for banner in category_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
 
   for category in CATEGORY_DB:
-    return render_template(category+".html", result=result, pagination=pagination)
+    return render_template(category+".html", 
+                            result=data('banners', category_banners), 
+                            pagination=pagination)
 
+
+#api for front-end
 @app.route('/api/banner', methods=['GET'])
 def banner():
   all_banners = Event.query.all()
-
-  result = []
-  for banner in all_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
+  result = data('banners', all_banners)
   return jsonify(result)
+
 
 @app.route('/api/home_banner', methods=['GET'])
 def get_home_banner():
   home_banners = Event.query.filter_by(status=1).filter_by(home_banner=1).all()
-  
-  result = []
-  for banner in home_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
+  result = data('banners', home_banners)
   return jsonify(result)
 
 
@@ -163,19 +91,44 @@ def get_category_banner(category):
 
   category_banners = Event.query.filter_by(
       status=1).filter_by(category_banner=1).filter_by(category=category).all()
-
-  result = []
-  for banner in category_banners:
-    data = {
-        "index": banner.id,
-        "img": banner.img,
-        "title": banner.title,
-        "display_date": banner.display_date,
-        "address": banner.location,
-        "desc": banner.desc
-    }
-    result.append(data)
+  result = data('banners', category_banners)
   return jsonify(result)
+
+
+@app.route('/api/events/<category>/<int:year>/<int:month>/<int:day>', methods=['GET'])
+def filter_events(category, year, month, day):
+
+  if category not in CATEGORY_DB:
+    return 'Category is not found.', 400
+  
+  try:
+    url_start_date = date(year, month, day)
+  except ValueError as n:
+    print(n)
+    return 'The date is not correct.', 400
+
+  url_end_date = url_start_date + timedelta(days=7)
+  events = Event.query.\
+      filter_by(category=category).\
+      filter(Event.end_date >= url_start_date).\
+      filter(Event.start_date <= url_end_date).\
+      all()
+  
+  result = data('filter_events', events)
+  return jsonify(result)
+
+
+@app.route('/api/<category>/banner', methods=['GET'])
+def get_banner(category):
+  if category not in CATEGORY_DB:
+    return 'Category is not found.', 400
+
+  event_banners = Event.query.filter_by(category=category).all()
+  if event_banners == None:
+      return {}
+  result = data('banner', event_banners)
+  return jsonify(result)
+
 
 #update event
 @app.route('/api/event/<int:id>', methods=['GET', 'PUT'])
@@ -183,35 +136,10 @@ def each_event(id):
   event = Event.query.filter_by(id=id).first()
 
   if request.method == 'GET':
-    event_category = event.category
-    result = []
-    data = {
-        "index": event.id,
-        "img": event.img,
-        "title": event.title,
-        "category": event_category.name,
-        "region": event.region,
-        "date": {
-            "start": event.start_date.strftime("%Y-%m-%d"),
-            "end": event.end_date.strftime("%Y-%m-%d"),
-        },
-        "display_date": event.display_date,
-        "note": event.note,
-        "address": event.location,
-        "link": event.link,
-        "desc": event.desc,
-        "reporter": {
-            "name": event.reporter_name,
-            "email": event.reporter_email,
-            "phone": event.reporter_phone,
-        },
-        "status": event.status,
-        "banner":{
-          "home": event.home_banner,
-          "category": event.category_banner,
-        }
-    }
-    result.append(data)
+    if event == None: 
+      return {}
+
+    result = data('event', event)
     return jsonify(result)
 
   if request.method == 'PUT':
@@ -232,67 +160,9 @@ def each_event(id):
     db.session.commit()
   return "update success!"
 
-@app.route('/api/events/<category>/<int:year>/<int:month>/<int:day>', methods=['GET'])
-def get_events(category, year, month, day):
-
-  if category not in CATEGORY_DB:
-    return 'Category is not found.', 400
-  
-  try:
-    url_start_date = date(year, month, day)
-  except ValueError as n:
-    print(n)
-    return 'The date is not correct.', 400
-
-  url_end_date = url_start_date + timedelta(days=7)
-  events = Event.query.\
-      filter_by(category=category).\
-      filter(Event.end_date >= url_start_date).\
-      filter(Event.start_date <= url_end_date).\
-      all()
-  
-  result = []
-  for event in events:
-    data = {
-      "img": event.img,
-      "title": event.title,
-      "region": event.region,
-      "date": {
-          "start": event.start_date.strftime("%Y-%m-%d"),
-          "end": event.end_date.strftime("%Y-%m-%d"),
-      },
-      "display_date": event.display_date,
-      "address": event.location,
-      "link": event.link,
-      "desc": event.desc
-    }
-    result.append(data)
-  return jsonify(result)
-
-@app.route('/api/<category>/banner', methods=['GET'])
-def get_banner(category):
-   
-  if category not in CATEGORY_DB:
-    return 'Category is not found.', 400
-
-  event_banners = Event.query.filter_by(category=category).all()
-
-  result = []
-  for banner in event_banners:
-    data = {
-      "index": banner.id,
-      "img": banner.img,
-      "title": banner.title,
-      "display_date": banner.display_date,
-      "address": banner.location,
-      "desc": banner.desc
-    }
-    result.append(data)
-  return jsonify(result)
 
 @app.route('/api/event', methods=['GET', 'POST'])
 def create_event():
-
   if request.method == 'POST':
     new_event = Event(
       title=request.json['title'],
@@ -323,42 +193,11 @@ def create_event():
       new_event.img = decode_and_get_url(
           request.json['img'], new_event.id)
       db.session.commit()
-
     return 'Saved to the database!'
 
   if request.method == 'GET':
     all_events = Event.query.all()
-
-    result = []
-    for event in all_events:
-      event_category = event.category
-      data = {
-        "index": event.id,
-        "img": event.img,
-        "title": event.title,
-        "category": event_category.name,
-        "region": event.region,
-        "date": {
-          "start": event.start_date.strftime("%Y-%m-%d"),
-          "end": event.end_date.strftime("%Y-%m-%d"),
-        },
-        "display_date": event.display_date,
-        "note": event.note,
-        "address": event.location,
-        "link": event.link,
-        "desc": event.desc,
-        "reporter": {
-          "name": event.reporter_name,
-          "email": event.reporter_email,
-          "phone": event.reporter_phone
-        },
-        "status": event.status,
-        "banner":{
-          "home": event.home_banner,
-          "category": event.category_banner,
-        }
-      }
-      result.append(data)
+    result = data('events', all_events)
     return jsonify(result)      
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,76 @@
+from structure import filter_events, event, banner
+
+def data(data_type, all_data):
+  result = []
+
+  if data_type == "event":
+    each_event_category = all_data.category
+    result.append(event(all_data.id,
+                        all_data.img,
+                        all_data.title,
+                        each_event_category.name,
+                        all_data.region,
+                        all_data.start_date.strftime("%Y-%m-%d"),
+                        all_data.end_date.strftime("%Y-%m-%d"),
+                        all_data.display_date,
+                        all_data.note,
+                        all_data.location,
+                        all_data.link,
+                        all_data.desc,
+                        all_data.reporter_name,
+                        all_data.reporter_email,
+                        all_data.reporter_phone,
+                        all_data.status,
+                        all_data.home_banner,
+                        all_data.category_banner)
+                  )
+
+  elif data_type == 'events':
+    for each_data in all_data:
+      each_event_category = each_data.category
+      result.append(event(each_data.id,
+                        each_data.img,
+                        each_data.title,
+                        each_event_category.name,
+                        each_data.region,
+                        each_data.start_date.strftime("%Y-%m-%d"),
+                        each_data.end_date.strftime("%Y-%m-%d"),
+                        each_data.display_date,
+                        each_data.note,
+                        each_data.location,
+                        each_data.link,
+                        each_data.desc,
+                        each_data.reporter_name,
+                        each_data.reporter_email,
+                        each_data.reporter_phone,
+                        each_data.status,
+                        each_data.home_banner,
+                        each_data.category_banner,
+                        )
+                    )
+
+  elif data_type == "filter_events":
+    for each_data in all_data:
+      result.append(filter_events(each_data.img,
+                        each_data.title,
+                        each_data.region,
+                        each_data.start_date.strftime("%Y-%m-%d"),
+                        each_data.end_date.strftime("%Y-%m-%d"),
+                        each_data.display_date,
+                        each_data.location,
+                        each_data.link,
+                        each_data.desc,
+                        )
+                    )
+
+  elif data_type == "banners":
+    for each_data in all_data:
+      result.append(banner(each_data.id,
+                        each_data.img,
+                        each_data.title,
+                        each_data.display_date,
+                        each_data.location,
+                        each_data.desc,
+                        )
+                    )
+  return result
