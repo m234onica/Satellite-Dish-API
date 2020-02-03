b1f1bfc96e089e920fb53817aa20f3f3eb0c4fee
joycehsu <m234onica@gmail.com>
2020-02-03 19:08:23 +0800

    bugfix for issue: home banner empty

--- src/route/api.py
+++ src/route/api.py
@@ -1,5 +1,5 @@
 from flask import (Flask, request, jsonify, render_template, Blueprint,
-                  current_app, url_for, redirect)
+                   current_app, url_for, redirect)
 
 from datetime import date, timedelta, datetime
 from enum import Enum
@@ -15,16 +15,10 @@ api = Blueprint('api', __name__)
 
 
 @api.route('/api/banner', methods=['GET'])
-def banner():
-  all_banners = Event.query.filter_by(status=1).\
-      filter(Event.show_banner != "hide").all()
-  result = data('banners', all_banners)
-  return jsonify(result)
-
-
-@api.route('/api/home_banner', methods=['GET'])
 def get_home_banner():
-  home_banners = Event.query.filter_by(status=1).filter_by(home_banner=1).all()
+  home_banners = Event.query.filter_by(status=1).\
+      filter(Event.show_banner != "hide").\
+      filter(Event.show_banner != "category").all()
   result = data('banners', home_banners)
   return jsonify(result)
 
@@ -45,7 +39,7 @@ def filter_events(category, year, month, day):
 
   if category not in CATEGORY_DB:
     return 'Category is not found.', 400
-  
+
   try:
     url_start_date = date(year, month, day)
   except ValueError as n:
@@ -58,7 +52,7 @@ def filter_events(category, year, month, day):
       filter(Event.end_date >= url_start_date).\
       filter(Event.start_date <= url_end_date).\
       all()
-  
+
   result = data('filter_events', events)
   return jsonify(result)
 
@@ -81,23 +75,23 @@ def each_event(id):
   event = Event.query.filter_by(id=id).first()
 
   if request.method == 'GET':
-    if event == None: 
+    if event == None:
       return {}
 
     result = data('event', event)
     return jsonify(result)
 
   if request.method == 'POST':
-    event.title=request.json['title']
-    event.category=request.json['category']
-    event.link=request.json['link']
+    event.title = request.json['title']
+    event.category = request.json['category']
+    event.link = request.json['link']
     event.description = request.json['description']
     event.region = request.json['region']
     event.start_date = request.json['start_date']
     event.end_date = request.json['end_date']
     event.display_date = request.json['display_date']
     event.location = request.json['location']
-    event.note=request.json['note']
+    event.note = request.json['note']
     event.status = request.json['status']
     event.show_banner = request.json['show_banner']
 
@@ -109,28 +103,28 @@ def each_event(id):
 def create_event():
   if request.method == 'POST':
     new_event = Event(
-      title=request.json['title'],
-      category=category_Enum(request.json['category']),
-      img="",
-      link=request.json['link'],
-      created_at=datetime.now(),
-      description=request.json['description'],
-      region=region_Enum(0),
-      start_date=request.json['date']['start'],
-      end_date=request.json['date']['end'],
-      display_date=request.json['display_date'],
-      location=request.json['location'],
-      note=request.json['note'],
-      reporter_name=request.json['reporter']['name'],
-      reporter_email=request.json['reporter']['email'],
-      reporter_phone=request.json['reporter']['phone'],
-      status=None,
-      show_banner=banner_Enum(0)
-      )
-    
+        title=request.json['title'],
+        category=category_Enum(request.json['category']),
+        img="",
+        link=request.json['link'],
+        created_at=datetime.now(),
+        description=request.json['description'],
+        region=region_Enum(0),
+        start_date=request.json['date']['start'],
+        end_date=request.json['date']['end'],
+        display_date=request.json['display_date'],
+        location=request.json['location'],
+        note=request.json['note'],
+        reporter_name=request.json['reporter']['name'],
+        reporter_email=request.json['reporter']['email'],
+        reporter_phone=request.json['reporter']['phone'],
+        status=None,
+        show_banner=banner_Enum(0)
+    )
+
     db.session.add(new_event)
     db.session.commit()
-    
+
     if new_event.img == "":
       new_event = Event.query.filter_by(id=new_event.id).first()
       new_event.img = decode_and_get_url(
@@ -141,4 +135,4 @@ def create_event():
   if request.method == 'GET':
     all_events = Event.query.all()
     result = data('events', all_events)
-    return jsonify(result)      
+    return jsonify(result)
