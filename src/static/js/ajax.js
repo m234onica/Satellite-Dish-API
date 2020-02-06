369e7dfe94e0b620feadb704d2560ae38afa36c5
joycehsu <m234onica@gmail.com>
2020-02-06 15:46:48 +0800

    change variable name of BASEURL

--- src/static/js/ajax.js
+++ src/static/js/ajax.js
@@ -1,10 +1,10 @@
 //get event data in modal
-$('.event_detail_button').click(function() {
+$('.event_detail_button').click(function () {
   var eventId = $(this).find('.detail_icon').attr('data-id');
-  
+
   $.ajax({
     type: "GET",
-    url: BASEURL + "/api/event/" + eventId,
+    url: base_url + "/api/event/" + eventId,
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
   }).done(function (data) {
@@ -24,19 +24,19 @@ $('.event_detail_button').click(function() {
     $('#eventCategory').val(data[0].category)
     $('#eventRegion').val(data[0].region)
 
-    
+
     if (data[0].show_banner == "all") {
       $('#eventHomeBanner').attr('checked', true)
       $('#eventCategoryBanner').attr('checked', true)
-      
-    }else if (data[0].show_banner == "home") {
+
+    } else if (data[0].show_banner == "home") {
       $('#eventHomeBanner').attr('checked', true)
       $('#eventCategoryBanner').attr('checked', false)
-      
+
     } else if (data[0].show_banner == "category") {
       $('#eventHomeBanner').attr('checked', false)
       $('#eventCategoryBanner').attr('checked', true)
-    } 
+    }
   })
 })
 
@@ -61,7 +61,7 @@ $('.accept_button').click(function () {
 
   var checkedHome = $("#eventHomeBanner").prop('checked')
   var checkedCate = $("#eventCategoryBanner").prop('checked')
-  
+
 
   if (checkedHome && !checkedCate) {
     Data.show_banner = "home";
@@ -69,16 +69,16 @@ $('.accept_button').click(function () {
     Data.show_banner = "category"
   } else if (checkedCate && checkedCate) {
     Data.show_banner = "all"
-  } 
-  
+  }
+
   $.ajax({
     type: "POST",
-    url: BASEURL + "/api/event/" + eventId,
+    url: base_url + "/api/event/" + eventId,
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
     data: JSON.stringify(Data),
   }).done(function (data) {
-    window.location.reload()    
+    window.location.reload()
   })
 })
 
@@ -102,22 +102,22 @@ $('.reject_button').click(function () {
 
   $.ajax({
     type: "POST",
-    url: BASEURL + "/api/event/" + eventId,
+    url: base_url + "/api/event/" + eventId,
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
     data: JSON.stringify(Data)
-  }).done(function(data) {
+  }).done(function (data) {
     window.location.reload()
   })
 })
 
 //get banner image
-$('.banner_image_button').click(function() {
+$('.banner_image_button').click(function () {
   var imageId = $(this).find('.image_icon').attr('data-id');
-  
+
   $.ajax({
     type: "GET",
-    url: BASEURL + "/api/event/" + imageId,
+    url: base_url + "/api/event/" + imageId,
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
   }).done(function (data) {
